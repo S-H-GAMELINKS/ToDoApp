06132e25fb17e931889134faed348b67e2079eee
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-10 12:10:59 +0900

    fix environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 
+environment.loaders.get('vue').options.extractCSS = false
 environment.loaders.append('vue', vue)
 module.exports = environment
 
-environment.loaders.get('vue').options.extractCSS = false
