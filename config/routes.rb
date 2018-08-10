f12b906fb05cbfc9f6b64c555638e935b9108263
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-10 11:13:34 +0900

    fix path(root, about, contact)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
+  get '/about', to: 'home#index'
+  get '/contact', to: 'home#index'
   
   namespace :api, format: 'json' do
     resources :tasks, only: [:index, :create, :update]
